8aaf8f9ac0c44339d94271d1c471ab9d06f2217d
Jack Youstra <jack@youstra.com>
2023-03-09 21:51:31 -0600

    Bump MetalPetal

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
        .package(
             name: "MetalPetal",
             url: "https://github.com/MetalPetal/MetalPetal",
-            from: "1.24.2"
+            from: "1.25.2"
        )
     ],
     targets: [
